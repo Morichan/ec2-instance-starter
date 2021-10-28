b530feeece55ecaffaadf289e6089acf808b4698
Morichan <morichan.keisuke.mori@gmail.com>
2021-10-28 19:18:48 +0900

    :memo: docs(docstring): docstringの日本語化および整備
    不要と判断したdocstringは除去した。

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,28 @@
 import json
 
-# import requests
-
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    """Lambda関数ハンドラー
 
     Parameters
     ----------
     event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        Lambdaプロキシ統合のための入力フォーマット
+        https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
     context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+        実行情報を提供するLambdaコンテキスト
+        https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
     Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    -------
+    Lambdaプロキシ統合のための出力フォーマット: dict
+        https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }
